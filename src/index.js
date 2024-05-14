7b96e3052e51c2c488c1e374879335bf2002a0f0
Pablo Cardozo <pablocyc83@gmail.com>
2024-05-13 23:24:20 -0400

    cuentaKilometros

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-class UserCard extends HTMLElement {
-  constructor() {
-    super();
-    const name = this.getAttribute("name");
-    const html = /* html */`
-      <div class="card">
-        <img src="https://avatars.githubusercontent.com/${name}?size=128" alt="${name}">
-        <span>${name}</span>
-      </div>
-    `;
+const COLORS = ["#e00", "#0e0", "#00e", "#ee0", "#0ee"];
 
-    this.insertAdjacentHTML("beforeend", html);
-  }
+const counter = document.querySelector(".counter");
+const digits = document.querySelector(".digits");
+let count = 0;
+let currentTheme = 0;
+
+const updateCounter = () => {
+  digits.textContent = count.toString().padStart(6, "0");
+};
+
+const incrementCounter = () => {
+  count++;
+  updateCounter();
+};
+
+const resetCounter = (ev) => {
+  ev.preventDefault();
+  count = 0;
+  updateCounter();
+};
+
+const changeTheme = () => {
+  currentTheme = (currentTheme + 1) % COLORS.length;
+  counter.style.setProperty("--color", COLORS[currentTheme]);
 }
 
-customElements.define("user-card", UserCard);
+document.addEventListener("pointermove", incrementCounter);
+document.addEventListener("contextmenu", resetCounter);
+document.addEventListener("click", changeTheme);
